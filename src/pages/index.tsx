a28d86c3ea6ab2093b8f24c7a96181b7932703e2
snowjang24 <asdka4618@gmail.com>
2022-12-28 14:29:55 +0900

    feat: emotion added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import { css } from "@emotion/react";
+
+export default function HomePage() {
+  return (
+    <div
+      css={css`
+        font-size: 2rem;
+      `}
+    >
+      Home Page
+    </div>
+  );
 }
